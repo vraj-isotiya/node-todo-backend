566dc2092bd92675ee176354af9ac51c80c93622
vraj-isotiya <vraj.isotiya@triyanshitechnologies.com>
2025-12-30 15:13:03 +0530

    initail commit

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
-import mongoose from "mongoose";
+import { connectDB } from "./db.js";
 import app from "./app.js";
 
-const startServer = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  } catch (err) {
-    console.error("Server startup failed", err);
-    process.exit(1);
-  }
-};
-
-startServer();
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on ${process.env.PORT}`);
+});
