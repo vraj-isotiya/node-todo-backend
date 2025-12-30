a0ab71ed2c388dedb57a54b42397920d510158ab
vraj-isotiya <vraj.isotiya@triyanshitechnologies.com>
2025-12-30 15:13:16 +0530

    initail commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import { connectDB } from "./db.js";
 import app from "./app.js";
 
+connectDB();
 app.listen(process.env.PORT, () => {
   console.log(`Server running on ${process.env.PORT}`);
 });
