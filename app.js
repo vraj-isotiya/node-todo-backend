cdf275aa10215f0eec7e74a29303675a279d37d2
vraj-isotiya <vraj.isotiya@triyanshitechnologies.com>
2025-12-30 14:40:08 +0530

    initail commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json());
 
 app.use("/api/health", healthRoutes);
 app.use("/api/todos", todoRoutes);
